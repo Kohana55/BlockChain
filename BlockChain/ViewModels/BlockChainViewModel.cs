7f98ba1e9de87592373591ff53803074962cc65f
Lewis Jenkins <lewis@kohana.co>
2019-03-03 23:06:01 +0000

    Added Tcp Client/Server wrappers with example code from documentation

--- BlockChain/ViewModels/BlockChainViewModel.cs
+++ BlockChain/ViewModels/BlockChainViewModel.cs
@@ -1,13 +1,7 @@
 ﻿using BlockChain.Models.BlockChain;
 using BlockChain.UtilityObjects;
-using System;
-using System.Collections.Generic;
 using System.Collections.ObjectModel;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Input;
 
 namespace BlockChain.ViewModels
 {
